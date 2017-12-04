1c6688eaacda52e528b9d099b4d6fb9ff7b0304b
Jason Ward <jason.ward@policystat.com>
2017-12-04 13:18:09 -0500

    refs #1: Updated to support django 1.11

--- ajax_select/urls.py
+++ ajax_select/urls.py
@@ -1,15 +1,17 @@
+from django.conf.urls import url
 
-from django.conf.urls.defaults import patterns, url
+import ajax_select.views
 
 
-urlpatterns = patterns('',
-    url(r'^ajax_lookup/(?P<channel>[-\w]+)$',
-        'ajax_select.views.ajax_lookup',
-        name = 'ajax_lookup'
+urlpatterns = [
+    url(
+        r'^ajax_lookup/(?P<channel>[-\w]+)$',
+        ajax_select.views.ajax_lookup,
+        name='ajax_lookup'
     ),
-    url(r'^add_popup/(?P<app_label>\w+)/(?P<model>\w+)$',
-        'ajax_select.views.add_popup',
-        name = 'add_popup'
+    url(
+        r'^add_popup/(?P<app_label>\w+)/(?P<model>\w+)$',
+        ajax_select.views.add_popup,
+        name='add_popup'
     )
-)
-
+]
